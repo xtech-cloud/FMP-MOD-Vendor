08a3a5b815dad505ef29857a766e1d17fa84957a
Easlee Liu <easlee@outlook.com>
2022-09-27 23:38:14 +0800

    regenerated

---
+++ vs2022/fmp-xtc-vendor-lib-mvcs/Permissions.cs
@@ -0,0 +1,18 @@
+
+namespace XTC.FMP.MOD.Vendor.LIB.MVCS
+{
+    public class Permissions
+    {
+
+        public const string HealthyCreate = "/XTC/Vendor/Healthy/Create";
+        public const string HealthyUpdate = "/XTC/Vendor/Healthy/Update";
+        public const string HealthyRetrieve = "/XTC/Vendor/Healthy/Retrieve";
+        public const string HealthyDelete = "/XTC/Vendor/Healthy/Delete";
+
+        public const string UnityCreate = "/XTC/Vendor/Unity/Create";
+        public const string UnityUpdate = "/XTC/Vendor/Unity/Update";
+        public const string UnityRetrieve = "/XTC/Vendor/Unity/Retrieve";
+        public const string UnityDelete = "/XTC/Vendor/Unity/Delete";
+
+    }
+}
